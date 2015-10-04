924ee2ac10a808d96a0311571ec48d797ddcf0ca
Kazuma Mori <deracine83.54@gmail.com>
2015-10-05 08:25:41 +0900

    Update imhist.R

--- R/imhist.R
+++ R/imhist.R
@@ -5,8 +5,9 @@
 #'
 #' @name imhistR
 #' @docType package
-#' @import Cairo downloader e1071 ggplot2 grid mmand patchPlot readbitmap rvest tidyr
+#' @import Cairo downloader e1071 ggplot2 grid mmand patchPlot readbitmap rvest
 #' @importFrom  scales scientific_format comma_format
 #' @importFrom  dplyr filter between select summarise mutate group_by
+#' @importFrom  tidyr gather
 NULL
 
